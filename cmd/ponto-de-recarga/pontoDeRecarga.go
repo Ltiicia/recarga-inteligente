f2dae9a741fd1759346351f1685b5ae18bcf4976
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2025-04-06 00:06:58 -0300

    ✨feat: add fila do ponto

--- cmd/ponto-de-recarga/pontoDeRecarga.go
+++ cmd/ponto-de-recarga/pontoDeRecarga.go
@@ -4,20 +4,61 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
+	"time"
+
 	"recarga-inteligente/internal/dataJson"
 	"recarga-inteligente/internal/logger"
 	"recarga-inteligente/internal/tcpIP"
 )
 
+var fila []string
+var mutex sync.Mutex
+
 func enviarDisponibilidade(logger *logger.Logger, conexao net.Conn) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	status := "Situacao atual: "
+	if len(fila) == 0 {
+		status += "sem fila"
+	} else {
+		status += fmt.Sprintf("com %d na fila", len(fila))
+	}
+
 	msg := dataJson.Mensagem{
 		Tipo:     "disponibilidade",
-		Conteudo: "Situacao atual: sem fila",
+		Conteudo: status,
 		Origem:   "ponto-de-recarga",
 	}
 	erro := dataJson.SendMessage(conexao, msg)
 	if erro != nil {
 		logger.Erro(fmt.Sprintf("Erro ao enviar disponibilidade - %v", erro))
+	}
+}
+
+func processarFila(logger *logger.Logger, conexao net.Conn) {
+	for {
+		mutex.Lock()
+		if len(fila) == 0 {
+			mutex.Unlock()
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		veiculo := fila[0]
+		fila = fila[1:]
+		mutex.Unlock()
+
+		logger.Info(fmt.Sprintf("Atendendo veículo: %s", veiculo))
+		time.Sleep(5 * time.Second) // Simula tempo de recarga
+		logger.Info(fmt.Sprintf("Recarga finalizada para: %s", veiculo))
+
+		msg := dataJson.Mensagem{
+			Tipo:     "recarga-finalizada",
+			Conteudo: fmt.Sprintf("Veículo %s atendido", veiculo),
+			Origem:   "ponto-de-recarga",
+		}
+		dataJson.SendMessage(conexao, msg)
 	}
 }
 
@@ -37,23 +78,32 @@ func main() {
 		logger.Erro("Erro em ConnectToServerTCP - ponto de recarga")
 		return
 	}
-	logger.Info("Ponto de Recarga conectado")
 	defer conexao.Close()
 	//envia identificacao inicial
+
+	logger.Info("Ponto de Recarga conectado")
 	IdentificacaoInicial(logger, conexao)
 
 	//recebe solicitacoes do servidor
+	go processarFila(logger, conexao)
+
 	for {
-		solicitacaoServidor, erro := dataJson.ReceiveMessage(conexao)
+		msg, erro := dataJson.ReceiveMessage(conexao)
 		if erro != nil {
 			logger.Erro(fmt.Sprintf("Erro ao ler mensagem do servidor - %v", erro))
 			return
 		}
 
-		if solicitacaoServidor.Tipo == "get-disponibilidade" {
+		switch msg.Tipo {
+		case "get-disponibilidade":
 			enviarDisponibilidade(logger, conexao)
-		} else {
-			logger.Info(fmt.Sprintf("Mensagem recebida do servidor: %s", solicitacaoServidor.Conteudo))
+		case "nova-solicitacao":
+			mutex.Lock()
+			fila = append(fila, msg.Conteudo)
+			logger.Info(fmt.Sprintf("Veículo adicionado à fila: %s", msg.Conteudo))
+			mutex.Unlock()
+		default:
+			logger.Info(fmt.Sprintf("Mensagem recebida do servidor: %s", msg.Conteudo))
 		}
 	}
 }
